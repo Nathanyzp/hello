4f302b5592d436d67c9f01d9bbb63994d7928a67
yuanzhipang <568624829@qq.com>
2019-09-06 10:51:32 +0800

    hhhh

---
+++ git_demo/src/com/yzp/comp/SayHi.java
@@ -0,0 +1,8 @@
+package com.yzp.comp;
+
+public class SayHi {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+}
---
+++ git_demo/src/com/yzp/comp/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Ntahn
+ *
+ */
+package com.yzp.comp;
